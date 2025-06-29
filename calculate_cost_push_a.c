e09bf79f03f65bb2a13bca485319a7342f34fe02
mandriaf <kevinzoldick202@gmail.com>
2025-06-29 21:26:30 +0300

    alefa amzay fa leo be

--- calculate_cost_push_a.c
+++ calculate_cost_push_a.c
@@ -6,7 +6,7 @@
 /*   By: mandriaf <mandriaf@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 10:59:15 by mandriaf          #+#    #+#             */
-/*   Updated: 2025/06/23 17:46:24 by mandriaf         ###   ########.fr       */
+/*   Updated: 2025/06/29 21:26:04 by mandriaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,14 @@ static int	best_value_push_a(t_stack *b)
 void	push_a_with_cost(t_stack **a, t_stack **b)
 {
 	int		best_value;
-	int		target_in_a;
 	int		cost_a;
 	int		cost_b;
 	t_stack	*best;
 
 	best_value = best_value_push_a(*b);
 	best = find_node(*b, best_value);
-	target_in_a = choice_target_to_a(*a, best);
 	cost_a = best->cost_a;
-	cost_b = get_cost(*a, target_in_a);
+	cost_b = best->cost_b;
 	double_move(a, b, &cost_a, &cost_b);
 	move_a_and_b(a, b, &cost_a, &cost_b);
 }
--- calculate_cost_push_b.c
+++ calculate_cost_push_b.c
@@ -6,7 +6,7 @@
 /*   By: mandriaf <mandriaf@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 16:04:28 by mandriaf          #+#    #+#             */
-/*   Updated: 2025/06/23 17:47:04 by mandriaf         ###   ########.fr       */
+/*   Updated: 2025/06/29 21:24:10 by mandriaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,14 @@ static int	best_value_push_b(t_stack *a)
 void	push_b_with_cost(t_stack **a, t_stack **b)
 {
 	int		best_value;
-	int		target_in_b;
 	int		cost_a;
 	int		cost_b;
 	t_stack	*best;
 
 	best_value = best_value_push_b(*a);
 	best = find_node(*a, best_value);
-	target_in_b = choice_target_to_b(best, *b);
 	cost_a = best->cost_a;
-	cost_b = get_cost(*b, target_in_b);
+	cost_b = best->cost_b;
 	double_move(a, b, &cost_a, &cost_b);
 	move_a_and_b(a, b, &cost_a, &cost_b);
 }

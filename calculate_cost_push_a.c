196208af2dc6f1cac1ad702dcb00608da847dab7
Manambina Kevin Andriafanomezantsoa <mandriaf@c1r6s5.42antananarivo.mg>
2025-06-30 11:05:35 +0300

    test

--- calculate_cost_push_a.c
+++ calculate_cost_push_a.c
@@ -6,26 +6,27 @@
 /*   By: mandriaf <mandriaf@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 10:59:15 by mandriaf          #+#    #+#             */
-/*   Updated: 2025/06/29 21:26:04 by mandriaf         ###   ########.fr       */
+/*   Updated: 2025/06/30 11:02:48 by mandriaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	best_value_push_a(t_stack *b)
+static t_stack	*best_value_push_a(t_stack *b)
 {
-	int	cost;
-	int	current;
-	int	best;
+	int		cost;
+	int		current;
+	t_stack	*best;
 
 	current = absolute_value(b->cost_a) + absolute_value(b->cost_b);
-	best = b->data;
+	best = b;
+	b = b->next;
 	while (b)
 	{
 		cost = absolute_value(b->cost_a) + absolute_value(b->cost_b);
 		if (cost < current)
 		{
-			best = b->data;
+			best = b;
 			current = cost;
 		}
 		b = b->next;
@@ -35,13 +36,11 @@ static int	best_value_push_a(t_stack *b)
 
 void	push_a_with_cost(t_stack **a, t_stack **b)
 {
-	int		best_value;
 	int		cost_a;
 	int		cost_b;
 	t_stack	*best;
 
-	best_value = best_value_push_a(*b);
-	best = find_node(*b, best_value);
+	best = best_value_push_a(*b);
 	cost_a = best->cost_a;
 	cost_b = best->cost_b;
 	double_move(a, b, &cost_a, &cost_b);
--- calculate_cost_push_b.c
+++ calculate_cost_push_b.c
@@ -6,26 +6,27 @@
 /*   By: mandriaf <mandriaf@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 16:04:28 by mandriaf          #+#    #+#             */
-/*   Updated: 2025/06/29 21:24:10 by mandriaf         ###   ########.fr       */
+/*   Updated: 2025/06/30 11:03:37 by mandriaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	best_value_push_b(t_stack *a)
+static t_stack	*best_value_push_b(t_stack *a)
 {
-	int	cost;
-	int	current;
-	int	best;
+	int		cost;
+	int		current;
+	t_stack	*best;
 
 	current = absolute_value(a->cost_a) + absolute_value(a->cost_b);
-	best = a->data;
+	best = a;
+	a = a->next;
 	while (a)
 	{
 		cost = absolute_value(a->cost_a) + absolute_value(a->cost_b);
 		if (cost < current)
 		{
-			best = a->data;
+			best = a;
 			current = cost;
 		}
 		a = a->next;
@@ -35,13 +36,11 @@ static int	best_value_push_b(t_stack *a)
 
 void	push_b_with_cost(t_stack **a, t_stack **b)
 {
-	int		best_value;
 	int		cost_a;
 	int		cost_b;
 	t_stack	*best;
 
-	best_value = best_value_push_b(*a);
-	best = find_node(*a, best_value);
+	best = best_value_push_b(*a);
 	cost_a = best->cost_a;
 	cost_b = best->cost_b;
 	double_move(a, b, &cost_a, &cost_b);
